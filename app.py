d68a7c5a134a01199d1ecbf93f07979b3ff5d0ec
Humol-e <luisemiliano.castroreza@gmail.com>
2025-03-01 13:50:43 -0700

    added inputs for search terms and naming of the csv file

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 url='https://listado.mercadolibre.com.mx/google-pixel-7#D[A:google%20pixel%207]]'
+
+baseurl = 'https://listado.mercadolibre.com.mx/'
+
+searchurl = input("Enter search term: ")
+csvname = input("Enter csv name: ")
+searchurl = searchurl.replace('-', '.').lower()
+url = baseurl + searchurl
+print(url)
+
 r=requests.get(url,headers=headers)
 print(r)
 
@@ -12,6 +21,8 @@ soup = BeautifulSoup(r.content, 'html.parser')
 
 items=[]
 print('aa')
+
+
 
 #for item in soup.select('ui-search-layout ui-search-layout--stack shops__layout'):
 for row in soup.find_all('ol', class_='ui-search-layout ui-search-layout--stack shops__layout'):
@@ -27,14 +38,17 @@ for row in soup.find_all('ol', class_='ui-search-layout ui-search-layout--stack 
         items.append(item)
     if not cards:
         print('No cards found')
-filename = 'tabla.csv'
-with open(filename, 'w', newline='', encoding='utf-8') as f:
+
+filename = csvname + '.csv'
+with open(r'./data/' + filename, 'w', newline='', encoding='utf-8') as f:
     w = csv.DictWriter(f,['name','price','discount','url','img'])
     w.writeheader()
     for item in items:
         w.writerow(item)
 
 
-with open("SOUP.html", "w", encoding="utf-8") as f:
+
+
+with open(r"data/SOUP.html", "w", encoding="utf-8") as f:
     f.write(soup.prettify())
 
