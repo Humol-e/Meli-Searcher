55e6bded38423eabb4cd0af9ec82fa93546287e8
Humol-e <luisemiliano.castroreza@gmail.com>
2025-03-01 18:37:48 -0700

    added a scraper for a single item, later will implement a way to store data and compare it from time to time

---
+++ itemtracker.py
@@ -0,0 +1,48 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url='https://www.mercadolibre.com.mx/motorola-moto-g34-5g-128-gb-azul-artico-8-gb-ram/p/MLM39407682?pdp_filters=item_id:MLM2120280109#is_advertising=true&searchVariation=MLM39407682&position=1&search_layout=stack&type=pad&tracking_id=556e6f78-2a3e-4ff1-99e8-6935288ab519&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=YjI3YjI4ZGYtNjhkYy00ZDdmLWE2MmItZjY3MGQ3MzlhYzY1'
+
+url = input("Enter url: ")
+#csvname = input("Enter csv name: ")
+csvname = "prueba"
+r=requests.get(url,headers=headers)
+print(r)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+items=[]
+
+for row in soup.find_all('div', class_='ui-pdp-container__row ui-pdp-with--separator--fluid ui-pdp-with--separator--40-24'):
+    cards = row.find_all('div', class_='ui-pdp-container__col col-2 mr-24 mt-8')
+    for card in cards:
+        item = {}
+        item['name'] = card.find('h1').text
+        price = card.find('div', class_ = 'ui-pdp-container__row ui-pdp-container__row--price')
+        if price:
+            item['price'] = price.find('span', class_='andes-money-amount__fraction').text.strip()
+        else:
+            item['price'] = 'N/A'
+        item['price'] = item['price'].replace('"', '').replace(',', '')
+        items.append(item)
+    if not cards:
+        print('No cards found')
+
+filename = csvname + '.csv'
+with open(r'./data/' + filename, 'w', newline='', encoding='utf-8') as f:
+    w = csv.DictWriter(f,['name','price'])
+    w.writeheader()
+    for item in items:
+        w.writerow(item)
+
+
+
+
+
+
+with open(r"data/item.html", "w", encoding="utf-8") as f:
+    f.write(soup.prettify())
+
