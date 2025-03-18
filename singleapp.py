1c2c4440ef49368a9ca9a607597143906c671fb5
Humol-e <luisemiliano.castroreza@gmail.com>
2025-03-18 13:27:29 -0700

    scraps an item in mercado libre, will start later to searh in another platforms.

---
+++ singleapp.py
@@ -0,0 +1,53 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url='https://listado.mercadolibre.com.mx/google-pixel-7#D[A:google%20pixel%207]]'
+
+baseurl = 'https://listado.mercadolibre.com.mx/'
+
+searchurl = input("Enter search term: ")
+csvname = input("Enter csv name: ")
+searchurl = searchurl.replace('-', '.').lower()
+url = baseurl + searchurl
+print(url)
+
+r=requests.get(url,headers=headers)
+print(r)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+items=[]
+print('aa')
+#for item in soup.select('ui-search-layout ui-search-layout--stack shops__layout'):
+for row in soup.find_all('ol', class_='ui-search-layout ui-search-layout--stack shops__layout'):
+    
+    cards = row.find_all('li', class_='ui-search-layout__item shops__layout-item')
+    for card in cards:
+
+        item = {}
+        item['name'] = card.find( 'h3').text
+        price_span = card.find('span', class_='andes-money-amount__fraction')
+
+        item['price'] = price_span.text.strip().replace(',', '') if price_span else 'N/A'
+        item['url'] = card.find('a', class_ = 'poly-component__title')['href']
+        item['img'] = card.find('img', class_='poly-component__picture')['src']
+        discount_span = card.find('span', class_='andes-money-amount__discount')
+        item['discount'] = discount_span.text.strip() if discount_span else 'N/A'
+        items.append(item)
+    if not cards:
+        print('No cards found')
+
+filename = csvname + '.csv'
+with open(r'./data/' + filename, 'w', newline='', encoding='utf-8') as f:
+    w = csv.DictWriter(f,['name','price','discount','url','img'])
+    w.writeheader()
+    for item in items:
+        w.writerow(item)
+
+
+
+
+with open(r"data/SOUP.html", "w", encoding="utf-8") as f:
+    f.write(soup.prettify())
